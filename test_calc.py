6558fb1380300ac0718e621dbc2237ac030534ad
Sahib Malhotra <sahibmalhotra@Sahibs-MBP.attlocal.net>
2019-12-22 21:55:47 -0800

    Unit Testing Performed

---
+++ test_calc.py
@@ -0,0 +1,16 @@
+import unittest
+import calc
+
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        result = calc.add(10, 5)
+        self.assertEqual(result, 15)
+
+    def test_subtract(self):
+        result = calc.subtract(10, 5)
+        self.assertEqual(result, 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
