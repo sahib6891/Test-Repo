851f342790c6105d9c0a0e3097cf789a4e449255
Sahib Malhotra <sahibmalhotra@Sahibs-MBP.attlocal.net>
2019-12-23 13:30:21 -0800

    Added Employee and test cases

--- test_calc.py
+++ test_calc.py
@@ -4,20 +4,24 @@ import calc
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        result = calc.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
 
     def test_subtract(self):
-        result = calc.subtract(10, 5)
-        self.assertEqual(result, 5)
+        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(-1, 1), -2)
+        self.assertEqual(calc.subtract(-1, -1), 0)
 
     def test_multiply(self):
-        result = calc.multiply(5, 5)
-        self.assertEqual(result, 25)
+        self.assertEqual(calc.multiply(10, 5), 50)
+        self.assertEqual(calc.multiply(-1, 1), -1)
+        self.assertEqual(calc.multiply(-1, -1), 1)
 
     def test_divide(self):
-        result = calc.divide(10, 2)
-        self.assertEqual(result, 5)
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(-1, -1), 1)
 
 
 if __name__ == "__main__":
